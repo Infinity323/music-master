97953f9d1fa5a208c2baafd711c6e95d472fba02
Nathaniel Trujillo <nathaniel.trujillo@tamu.edu>
2023-02-27 09:11:59 -0600

    added music sheet selection to recording

--- client/src/pages/StartPracticeSession.js
+++ client/src/pages/StartPracticeSession.js
@@ -3,11 +3,14 @@ import { RecordButton } from "../components/Buttons";
 import SheetMusicDropdown from "../components/SheetMusicDropdown";
 
 function StartPracticeSession() {
+
   return (
     <>
       <BackButton/>
       <div className="content">
         <p>start practice session</p>
+        <p>NOTICE: select music sheet before proceeding, no error checking is implemented ATM.</p>
+        <SheetMusicDropdown/>
         <RecordButton/>
       </div>
     </>
