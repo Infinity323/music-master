8bf8156e2f7f1ef45ad9658503d4228765d8d9f6
Nathaniel Trujillo <nathaniel.trujillo@tamu.edu>
2023-02-27 11:24:52 -0600

    added recording button disable on no-sheet-music-selected

--- client/src/pages/StartPracticeSession.js
+++ client/src/pages/StartPracticeSession.js
@@ -1,15 +1,28 @@
 import { BackButton } from "../components/Buttons";
-import { RecordButton } from "../components/Buttons";
 import SheetMusicDropdown from "../components/SheetMusicDropdown";
+import { useNavigate } from 'react-router-dom';
+import { useContext } from "react";
+import { SheetMusicIdContext } from "../App";
+import recording from '../assets/images/recording.png'
 
 function StartPracticeSession() {
+
+  function RecordButton() {
+    const navigate = useNavigate();
+    const sheetMusicId  = useContext(SheetMusicIdContext)[0];
+    return (
+      <div className={sheetMusicId === -1 ? "btn medium disabled" : "btn medium"} onClick={() => navigate("/recording")}>
+        <img src={recording} alt="Back" width="60px"/>
+      </div>
+    );
+  }
 
   return (
     <>
       <BackButton/>
       <div className="content">
-        <p>start practice session</p>
-        <p>NOTICE: select music sheet before proceeding, no error checking is implemented ATM.</p>
+        <h1>Practice Session</h1>
+        <p>Select the sheet music you want to record a performance of.</p>
         <SheetMusicDropdown/>
         <RecordButton/>
       </div>
