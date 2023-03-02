1f83ad2812e23eab1dd9e7e5bed845c498b4470a
Nathaniel Trujillo <nathaniel.trujillo@tamu.edu>
2023-03-02 10:23:48 -0600

    minor refatoring

--- server/api/mxl_wav_compare.py
+++ server/api/mxl_wav_compare.py
@@ -1,14 +1,22 @@
 from deepdiff import DeepDiff
 from deepdiff import DeepSearch
 
-def get_diffs(mxl_json, wav_json):
-    diff_raw = DeepDiff(mxl_json, wav_json) # https://zepworks.com/deepdiff/6.2.3/index.html
-    diff_paths = diff_raw.affected_paths # OrderedSet of paths that contain a diff
+# https://zepworks.com/deepdiff/6.2.3/index.html
+def get_diff_raw(mxl_json, wav_json):
+    diff_raw = DeepDiff(mxl_json, wav_json) 
+    return diff_raw
 
-    # dictionary with all feedback differences (keys and values)
-    diff_list = diff_raw.to_dict()
+ # dictionary with all feedback differences (keys and values)
+def get_diff_list(mxl_json, wav_json):
+    diff_list = get_diff_raw(mxl_json, wav_json).to_dict() 
+    return diff_list
 
-    return diff_paths, diff_list
+# attribute is str "pitch", "velocity", "start", or "end"
+def get_diff_indexes(mxl_json, wav_json, attribute: str):
+    # OrderedSet of paths that contain a diff
+    diff_paths = get_diff_raw(mxl_json, wav_json).affected_paths 
+    diff_indexes = DeepSearch(diff_paths, attribute, verbose_level=2) # index locations discrepencies
+    return diff_indexes
 
 def get_num_notes(json):
     return len(json["notes"])
@@ -24,9 +32,7 @@ def check_same_length(mxl_json, wav_json):
     
 # compares mxl and wav json file against eachother and returns diff
 def compare_tuning(mxl_json, wav_json):
-
-    diff_paths = get_diffs(mxl_json, wav_json)[0] # diff_paths
-    diff_list = get_diffs(mxl_json,  wav_json)[1] # diff_list
+    diff_list = get_diff_list(mxl_json,  wav_json) # diff_list
 
     if check_same_length(mxl_json, wav_json):
         num_notes = get_num_notes(mxl_json)
@@ -36,7 +42,7 @@ def compare_tuning(mxl_json, wav_json):
         # use original json key name to access value specifics from diff_list
 
         # get number of pitch discrepencies
-        pitch_diffs = DeepSearch(diff_paths, "pitch", verbose_level=2) # index locations of pitch discrepencies
+        pitch_diffs = get_diff_indexes(mxl_json, wav_json, "pitch") # index locations of pitch discrepencies
         if "matched_values" in pitch_diffs:
             num_pitch_diffs = len(pitch_diffs['matched_values']) # number of pitch discrepencies
             for index in pitch_diffs['matched_values']: # iterate through discrepencies and print each
@@ -50,16 +56,14 @@ def compare_tuning(mxl_json, wav_json):
     return new_tuning_percent_accuracy
 
 def compare_dynamics(mxl_json, wav_json):
-
-    diff_paths = get_diffs(mxl_json, wav_json)[0] # diff_paths
-    diff_list = get_diffs(mxl_json,  wav_json)[1] # diff_list
+    diff_list = get_diff_list(mxl_json,  wav_json) # diff_list
 
     if check_same_length(mxl_json, wav_json):
         num_notes = get_num_notes(mxl_json)
         print("note length matches!")
 
         # get number of dynamics discrepencies
-        dynamics_diffs = DeepSearch(diff_paths, "velocity", verbose_level=2) # index locations of dynamics discrepencies
+        dynamics_diffs = get_diff_indexes(mxl_json, wav_json, "velocity") # index locations of dynamics discrepencies
         if "matched_values" in dynamics_diffs: # check if any discrepencies were found
             num_dynamics_diffs = len(dynamics_diffs['matched_values']) # number of dynamics discrepencies
             for index in dynamics_diffs['matched_values']: # iterate through discrepencies and print each
@@ -71,17 +75,15 @@ def compare_dynamics(mxl_json, wav_json):
     return new_dynamics_percent_accuracy
 
 def compare_tempo(mxl_json, wav_json):
-
-    diff_paths = get_diffs(mxl_json, wav_json)[0] # diff_paths
-    diff_list = get_diffs(mxl_json,  wav_json)[1] # diff_list
+    diff_list = get_diff_list(mxl_json,  wav_json) # diff_list
 
     if check_same_length(mxl_json, wav_json):
         num_notes = get_num_notes(mxl_json)
         print("note length matches!")
 
         # get number of tempo discrepencies
-        tempo_start_diffs = DeepSearch(diff_paths, "start", verbose_level=2)
-        tempo_end_diffs = DeepSearch(diff_paths, "end", verbose_level=2)
+        tempo_start_diffs = get_diff_indexes(mxl_json, wav_json, "start")
+        tempo_end_diffs = get_diff_indexes(mxl_json, wav_json, "end")
         num_tempo_diffs = 0
         if "matched_values" in tempo_start_diffs:
             num_tempo_diffs += len(tempo_start_diffs['matched_values'])
