98786e330bc3ac59249ebc71c3c7e8555a2b1288
Infinity323 <paul_in_plano@hotmail.com>
2023-04-02 19:48:33 -0500

    Add clarinet sample

--- server/tests/test_signal_processing.py
+++ server/tests/test_signal_processing.py
@@ -94,3 +94,14 @@ def test_wet_hands_actual():
     assert(len(notes) == len(xml_notes))
     for i in range(len(notes)):
         assert(Note.frequency_difference_in_cents(notes[i].pitch, xml_notes[i].pitch) <= MAX_CENTS_ERROR)
+
+def test_gerudo_valley_expected():
+    # File exported from MuseScore
+    file = WAV_DATA_PATH + "gerudovalley_expected.wav"
+    f0, times, amp_maxes = get_f0_time_amp(file)
+    notes = freq_to_notes(f0, times, amp_maxes)
+    xml_notes = initialize_notes("Gerudo Valley.json")
+    
+    assert(len(notes) == len(xml_notes))
+    for i in range(len(notes)):
+        assert(Note.frequency_difference_in_cents(notes[i].pitch, xml_notes[i].pitch) <= MAX_CENTS_ERROR)
